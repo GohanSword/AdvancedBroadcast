1695046965e1fdfffcc3dba5c1c77af1b02f4f2b
GohanSword <charlii68200@gmail.com>
2020-10-28 10:27:14 +0100

    Push

--- src/main/java/fr/gohansword/advancedbroadcast/AdvancedBroadcast.java
+++ src/main/java/fr/gohansword/advancedbroadcast/AdvancedBroadcast.java
@@ -35,7 +35,7 @@ public final class AdvancedBroadcast extends JavaPlugin {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
         String newLine = System.getProperty("line.separator");
-        String blankline = "&f";
+        String blankline = "&c";
 
         if (label.equalsIgnoreCase("advancedbroadcast")) {
             if (args.length == 0) {
